08dae9e754f9bb9c5effc1c76eaba20b5c540747
Gökay Solman <gokaysolman@hotmail.com>
2022-07-13 15:27:48 +0300

    Service provider & directory structure

---
+++ app/RealEstate/Providers/RealEstateServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\RealEstate\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class RealEstateServiceProvider extends ServiceProvider
+{
+    /**
+     * @var string
+     */
+    protected $namespace = 'App\\RealEstate\\Controllers';
+
+    /**
+     * @return void
+     */
+    public function register()
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(app_path('RealEstate/Routes/api.php'));
+
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(app_path('RealEstate/Routes/web.php'));
+    }
+}
