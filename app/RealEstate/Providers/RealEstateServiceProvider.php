472700b75df6e381323b40dd83b942c5d8bcd4f6
Gökay Solman <gokaysolman@hotmail.com>
2022-07-21 14:16:10 +0300

    Services bind to container

--- app/RealEstate/Providers/RealEstateServiceProvider.php
+++ app/RealEstate/Providers/RealEstateServiceProvider.php
@@ -2,8 +2,17 @@
 
 namespace App\RealEstate\Providers;
 
+use App\RealEstate\Contracts\IDirectionResolver;
 use App\RealEstate\Contracts\IZipResolver;
+use App\RealEstate\Events\AppointmentCreated;
+use App\RealEstate\Events\AppointmentDeleted;
+use App\RealEstate\Events\AppointmentUpdated;
+use App\RealEstate\Events\PropertyCreated;
+use App\RealEstate\Events\PropertyDeleted;
+use App\RealEstate\Events\PropertyUpdated;
+use App\RealEstate\Services\GoogleDirections;
 use App\RealEstate\Services\PostCodesIO;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,12 +24,60 @@ class RealEstateServiceProvider extends ServiceProvider
     protected $namespace = 'App\\RealEstate\\Controllers';
 
     /**
+     * @var array[]
+     */
+    protected $events = [
+        PropertyCreated::class => [],
+        PropertyUpdated::class => [],
+        PropertyDeleted::class => [],
+        AppointmentCreated::class => [],
+        AppointmentUpdated::class => [],
+        AppointmentDeleted::class => []
+    ];
+
+    /**
      * @return void
      */
     public function register()
     {
+        $this->setRoutes();
+        $this->setEvents();
+        $this->bindDependencies();
         $this->loadMigrationsFrom(app_path('RealEstate/assets/migrations'));
+    }
 
+    /**
+     * @return void
+     */
+    protected function bindDependencies()
+    {
+        $this->app->bind(IZipResolver::class, function(){
+            return new PostCodesIO();
+        });
+
+        $this->app->bind(IDirectionResolver::class, function(){
+            return new GoogleDirections(env('GOOGLE_API_KEY'));
+        });
+
+    }
+
+    /**
+     * @return void
+     */
+    protected function setEvents()
+    {
+        foreach ($this->events as $event => $listeners) {
+            foreach($listeners as $listener) {
+                Event::listen($event, $listener);
+            }
+        }
+    }
+
+    /**
+     * @return void
+     */
+    protected function setRoutes()
+    {
         Route::prefix('api')
             ->middleware('api')
             ->namespace($this->namespace)
@@ -30,8 +87,5 @@ class RealEstateServiceProvider extends ServiceProvider
             ->namespace($this->namespace)
             ->group(app_path('RealEstate/assets/routes/web.php'));
 
-        $this->app->bind(IZipResolver::class, function(){
-            return new PostCodesIO();
-        });
     }
 }
